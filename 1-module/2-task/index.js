1361753c1917e1774d05024c6869afa9ba3826c3
Vasilisa Zaitseva <vasilisa.zaytseva@gmail.com>
2020-06-22 21:34:09 -0400

    1_module,2_task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,9 +10,13 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if ((name.includes(' ') == 0) && (name.length >= 4)) {
+    return true 
+  } else {
+    return false
+  }
 }
-
+  
 function sayHello() {
   let userName = prompt('Введите ваше имя');
 
