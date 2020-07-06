1e39044f91e48bd9fa9c0563eb9f83ef6ee3a414
Vasilisa Zaitseva <vasilisa.zaytseva@gmail.com>
2020-07-05 21:12:00 -0400

    3_module,5_task, max and min from the string

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,23 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+  let array = getNumbersArray(str);
+  let max = Math.max(...array);
+  let min = Math.min(...array);
+  result.min = min;
+  result.max = max;
+  return result;
+};
+
+
+function getNumbersArray(str) {
+  let result = [];
+  str.split(/,| /).forEach(item => {
+    if (isFinite(item) && item != "") {
+      result.push(+item)
+    }
+  });
+  return result;
 }
+
