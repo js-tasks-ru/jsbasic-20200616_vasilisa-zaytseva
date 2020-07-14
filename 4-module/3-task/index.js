dbc437119b92d0efb8069e3b0ef4188ea8c23a55
Vasilisa Zaitseva <vasilisa.zaytseva@gmail.com>
2020-07-13 21:53:19 -0400

    4_module,3_task, highlight the table

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,29 @@
  * @param {Element} table
  */
 function highlight(table) {
-}
+    let rows = table.rows;
+    for (let i = 0; i < rows.length; i++) {
+	
+	    let gender = rows[i].cells[2].innerHTML;
+        let status = rows[i].cells[3].dataset.available;
+        let age = +rows[i].cells[1].innerHTML;
+
+        if (gender === 'm') {
+            rows[i].className += ' male';
+        } else if (gender === 'f') {
+            rows[i].className += ' female';
+        }
+
+        if (status === 'true') {
+            rows[i].className += ' available';
+        } else if (status === 'false') {
+            rows[i].className += ' unavailable';
+        } else if (status == undefined) {
+            rows[i].setAttribute('hidden', 'true');
+        }
+
+        if (age < 18) {
+            rows[i].setAttribute('style', 'text-decoration: line-through');
+        }
+    }
+};
