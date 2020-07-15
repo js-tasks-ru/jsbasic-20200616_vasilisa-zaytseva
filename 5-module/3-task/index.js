7b6341c11f9c502e50df95fb48daa5bd31844e28
Vasilisa Zaitseva <vasilisa.zaytseva@gmail.com>
2020-07-15 18:50:18 -0400

    5_module,3_task, project-carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  
+  const carousel = document.querySelector('.carousel__inner');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const width = carousel.offsetWidth;
+
+  carousel.setAttribute('image_offset', 0);
+  arrowRight.style.display = '';
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', ()  => { 
+    let imageOffset = +carousel.getAttribute('image_offset');
+    arrowLeft.style.display = '';
+    if (imageOffset < 3){
+      let px = width * (imageOffset + 1);
+      carousel.style.transform = `translateX(-${px}px)`;
+      carousel.setAttribute('image_offset', imageOffset + 1);
+      if (imageOffset == 2) {
+        arrowRight.style.display = 'none';
+      }
+    }
+    }); 
+
+  arrowLeft.addEventListener('click', ()  => { 
+    let imageOffset = +carousel.getAttribute('image_offset');
+    if (imageOffset > 0) {
+      arrowRight.style.display = '';
+      let px = width * (imageOffset - 1);
+      carousel.style.transform = `translateX(-${px}px)`;
+      carousel.setAttribute('image_offset', imageOffset - 1);
+    }
+    if (imageOffset == 1) {
+      arrowLeft.style.display = 'none';
+    }
+  });
 }
