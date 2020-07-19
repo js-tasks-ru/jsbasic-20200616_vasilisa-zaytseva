ea17436b2686ca64c56c8185b411b0d3fb783a35
Vasilisa Zaitseva <vasilisa.zaytseva@gmail.com>
2020-07-18 22:58:46 -0400

    6_module,2_task, project: create class ProductCard

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,45 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    let elem = document.createElement('div');
+    elem.className = 'card';
+    
+    let cardTop = document.createElement('div');
+    elem.appendChild(cardTop);
+    cardTop.className = 'card__top';
+    let img = document.createElement('img');
+    cardTop.appendChild(img);
+    img.setAttribute('src', `/assets/images/products/${product.image}`);
+    img.className = 'card__image';
+    img.setAttribute('alt', 'product');
+    let span = document.createElement('span');
+    cardTop.appendChild(span);
+    span.className = 'card__price';
+    span.innerHTML = `€${product.price.toFixed(2)}`;
+    
+    let cardBody = document.createElement('div');
+    elem.appendChild(cardBody);
+    cardBody.className = 'card__body';
+    let cardTitle  = document.createElement('div');
+    cardBody.appendChild(cardTitle);
+    cardTitle.className = 'card__title';
+    cardTitle.innerHTML = `${product.name}`;
+    let button = document.createElement('button');
+    cardBody.appendChild(button);
+    button.className = 'card__button';
+    button.setAttribute('type', 'button');
+    let img2 = document.createElement('img');
+    button.appendChild(img2);
+    img2.setAttribute('src', '/assets/images/icons/plus-icon.svg');
+    img2.setAttribute('alt', 'icon');
+    button.addEventListener('click', () => {
+      let event = new CustomEvent("product-add", { 
+        detail: product.id, 
+        bubbles: true 
+      });
+      elem.dispatchEvent(event);
+    });
+
+    this.elem = elem;
   }
 }
